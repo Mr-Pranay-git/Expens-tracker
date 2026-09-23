a55b3f5d428dfeb8c50a92bb4020d198e3892631
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-09-23 23:56:09 +0530

    backend is connected with database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 app.listen(8080, ()=> console.log('Server is running on port 8080'));
 
+
+// database connection
+import mongoose from 'mongoose';
+mongoose.connect(process.env.DB_URL)
+.then(()=> console.log('Database connected !'))
+.catch(()=> console.log('Database Not connected !'))
 
 // app level middleware
 import morgan from 'morgan';
